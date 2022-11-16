9b29ad114eed025dae2ff3a22dfff5f74c75e981
Jonathas Magalhaes <jondejotam@gmail.com>
2022-11-16 02:15:30 -0300

    init

---
+++ src/main/java/dh/consultas/ConsultasApplication.java
@@ -0,0 +1,13 @@
+package dh.consultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultasApplication.class, args);
+    }
+
+}
