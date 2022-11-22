6b93aead57c95a85986991f747188da9583ac60c
Jonathas <jondejotam@gmail.com>
2022-11-22 19:38:16 -0300

    annotations

--- src/main/java/dh/consultas/model/Consulta.java
+++ src/main/java/dh/consultas/model/Consulta.java
@@ -1,7 +1,16 @@
 package dh.consultas.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Consulta {
     private Paciente paciente;
     private Dentista dentista;
--- src/main/java/dh/consultas/model/Pessoa.java
+++ src/main/java/dh/consultas/model/Pessoa.java
@@ -1,10 +1,14 @@
 package dh.consultas.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pessoa {
     private String nome;
     private String sobrenome;
