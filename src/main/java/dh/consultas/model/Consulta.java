dc6cdbf609fbbee756d659e2cfb63907a6e03bec
Jonathas <jondejotam@gmail.com>
2022-11-21 21:32:22 -0300

    create Consulta and ConsultaDTO

--- src/main/java/dh/consultas/model/Consulta.java
+++ src/main/java/dh/consultas/model/Consulta.java
@@ -1,4 +1,10 @@
 package dh.consultas.model;
 
+import java.time.LocalDateTime;
+
 public class Consulta {
+    private Paciente paciente;
+    private Dentista dentista;
+    private LocalDateTime horario;
+
 }
---
+++ src/main/java/dh/consultas/model/dto/ConsultaDTO.java
@@ -0,0 +1,10 @@
+package dh.consultas.model.dto;
+
+import java.time.LocalDateTime;
+
+public class ConsultaDTO {
+
+    private String paciente;
+    private String dentista;
+    private LocalDateTime horario;
+}
