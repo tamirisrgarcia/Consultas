b14456b5515fd448c6a7a1cae5e35263e54bbb67
Jonathas <jondejotam@gmail.com>
2022-11-23 23:26:39 -0300

    Update PacienteService.java

--- src/main/java/dh/consultas/service/PacienteService.java
+++ src/main/java/dh/consultas/service/PacienteService.java
@@ -1,7 +1,49 @@
 package dh.consultas.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dh.consultas.entity.Paciente;
+import dh.consultas.entity.dto.PacienteDTO;
+import dh.consultas.repository.PacienteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PacienteService {
+    final
+    PacienteRepository pacienteRepository;
+
+    public PacienteService(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    public List<PacienteDTO> listar() {
+
+        List<Paciente> list = pacienteRepository.findAll();
+        List<PacienteDTO> dto = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        for (Paciente paciente : list) {
+            PacienteDTO pacienteDTO = mapper.convertValue(paciente, PacienteDTO.class);
+            dto.add(pacienteDTO);
+        }
+        return dto;
+    }
+
+    public ResponseEntity<PacienteDTO> buscar(Long id) {
+
+        Optional<Paciente> optionalPaciente = pacienteRepository.findById(id);
+
+        if (optionalPaciente.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Paciente paciente = optionalPaciente.get();
+        PacienteDTO dto = mapper.convertValue(paciente, PacienteDTO.class);
+
+        return ResponseEntity.ok(dto);
+
+    }
 }
