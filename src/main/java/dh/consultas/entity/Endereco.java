4182802656847c7e85b709fa0d931f6a55654718
Felipe Arruda <felipe.berriel.arruda@gmail.com>
2022-11-28 18:13:55 -0300

    Update Endereco.java

--- src/main/java/dh/consultas/entity/Endereco.java
+++ src/main/java/dh/consultas/entity/Endereco.java
@@ -1,25 +1,38 @@
 package dh.consultas.entity;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Getter
-@Setter
-@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+@Entity @Table(name = "endereco")
+
 public class Endereco {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String rua;
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String cep;
+    private Long    id;
+
+    @Column
+    private String  rua;
+
+    @Column
+    private int     numero;
+
+    @Column
+    private String  complemento;
+
+    @Column
+    private String  bairro;
+
+    @Column
+    private String  cidade;
+
+    @Column
+    private String  estado;
+
+    @Column
+    private String  cep;
 }
