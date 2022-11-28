4c9fe2bee2d428d89232a4192fab8f922ff8d12e
Felipe Arruda <felipe.berriel.arruda@gmail.com>
2022-11-28 18:12:08 -0300

    Create EnderecoRepository.java

---
+++ src/main/java/dh/consultas/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package dh.consultas.repository;
+import dh.consultas.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository <Endereco, Long> {}
