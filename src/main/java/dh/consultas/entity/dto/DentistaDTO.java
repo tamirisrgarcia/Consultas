4393493a4905419fedd7a9edfd254e1ccb40115f
Felipe Arruda <felipe.berriel.arruda@gmail.com>
2022-12-12 19:04:13 -0300

    Update Dentista
    precisei colocar @AllArgsConstructor pra testar a consulta

--- src/main/java/dh/consultas/entity/dto/DentistaDTO.java
+++ src/main/java/dh/consultas/entity/dto/DentistaDTO.java
@@ -1,11 +1,15 @@
 package dh.consultas.entity.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DentistaDTO {
     private Long id;
