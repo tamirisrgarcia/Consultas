55390b79141ea526ec9c5c24d9fe13649d748de0
Felipe Arruda <felipe.berriel.arruda@gmail.com>
2022-12-12 19:06:31 -0300

    Update Paciente
    > Alterei os construtores
    > Alterei o tamanho máximo do RG de 9 pra 10

--- src/main/java/dh/consultas/entity/dto/PacienteDTO.java
+++ src/main/java/dh/consultas/entity/dto/PacienteDTO.java
@@ -1,11 +1,15 @@
 package dh.consultas.entity.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PacienteDTO {
 
@@ -13,4 +17,10 @@ public class PacienteDTO {
     private String sobrenome;
     private String rg;
     private String alta;
+
+    public PacienteDTO(String nome, String sobrenome, String rg) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+    }
 }
