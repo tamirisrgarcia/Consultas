bc5e258864309e6ce0fc6aa263d6bd4711065068
Jonathas <jondejotam@gmail.com>
2022-12-12 22:09:04 -0300

    Update PacienteServiceTest.java

--- src/test/java/dh/consultas/service/PacienteServiceTest.java
+++ src/test/java/dh/consultas/service/PacienteServiceTest.java
@@ -1,7 +1,10 @@
 package dh.consultas.service;
 
+import dh.consultas.entity.Paciente;
 import dh.consultas.entity.dto.PacienteDTO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -9,30 +12,20 @@ import java.util.List;
 @SpringBootTest
 class PacienteServiceTest {
 
+    @Autowired
+    PacienteService pacienteService;
+
     @Test
     void listar() {
-        List<PacienteDTO> pacientes = null;
-
-
-    }
-
-    @Test
-    void buscar() {
+        List<PacienteDTO> pacientes = pacienteService.listar();
+        Assertions.assertNotNull(pacientes);
     }
 
     @Test
     void salvar() {
+        Paciente paciente = new Paciente();
+        Assertions.assertNotNull(pacienteService.salvar(paciente));
+
     }
 
-    @Test
-    void substituir() {
-    }
-
-    @Test
-    void alterar() {
-    }
-
-    @Test
-    void deletar() {
-    }
 }
