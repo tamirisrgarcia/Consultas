3a643609d0536c75fc73d0d473c9a986c408204d
Jonathas <jondejotam@gmail.com>
2022-12-12 21:57:06 -0300

    Update SecurityConfiguration.java

--- src/main/java/dh/consultas/security/SecurityConfiguration.java
+++ src/main/java/dh/consultas/security/SecurityConfiguration.java
@@ -33,8 +33,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/pacientes").hasAuthority("admin")
-                .antMatchers("/dentista").hasAuthority("admin")
+                .antMatchers("/pacientes").hasRole("admin")
+                .antMatchers("/dentista").hasRole("admin")
                 .antMatchers("/consulta").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
